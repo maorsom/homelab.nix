ca16aaf0411e0c3b0d24a4de8efbd2ecc595c8a5
maorsom <maorsom@gmail.com>
2025-02-03 01:11:45 +0200

    test vm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,11 +76,11 @@
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
     # FIXME: Replace with your username
-    your-username = {
+    marosom = {
       # TODO: You can set an initial password for your user.
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "correcthorsebatterystaple";
+      initialPassword = "Oran1Ofir2!";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
